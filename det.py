f55ee145906cec3b768d48292e2a65519f9614cb
LuPei9ing <30286145+LuPei9ing@users.noreply.github.com>
2019-10-28 20:55:44 -0400

    Add files via upload

---
+++ det.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 16:45:01 2019
+
+@author: lenovo
+"""
+
+def det(m):
+    if len(m) <= 0:
+        return None
+    if len(m) == 1:
+        return m[0][0]
+    else:
+        s = 0
+        for i in range(len(m)):
+            n = [[row[a] for a in range(len(m)) if a != i] for row in m[1:]]
+            if i % 2 == 0:
+                s += m[0][i] * det(n)
+            else:
+                s -= m[0][i] * det(n)
+        return s
+    
